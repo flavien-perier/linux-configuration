703155693c7729997e8260f2f9be7188c5163298
Flavien PERIER <flavien.perier@pm.me>
2025-10-25 15:51:34 +0200

    Fix sway conf dir

--- xfce.sh
+++ xfce.sh
@@ -114,14 +114,14 @@ apply_tmux_settings() {
 }
 
 apply_sway_settings() {
-    local HOME_DIR=$1
+    local CONF_DIR=$1
 
     if command_exists "sway"
     then
-        mkdir -p $HOME_DIR/sway/config.d
-        curl $GITHUB_PROJECT_BASE_URL/sway/config > $HOME_DIR/sway/config
-        curl $GITHUB_PROJECT_BASE_URL/sway/config.d/keyboard > $HOME_DIR/sway/config.d/keyboard
-        curl $GITHUB_PROJECT_BASE_URL/sway/config.d/theme > $HOME_DIR/sway/config.d/theme
+        mkdir -p $CONF_DIR/config.d
+        curl $GITHUB_PROJECT_BASE_URL/sway/config > $CONF_DIR/config
+        curl $GITHUB_PROJECT_BASE_URL/sway/config.d/keyboard > $CONF_DIR/config.d/keyboard
+        curl $GITHUB_PROJECT_BASE_URL/sway/config.d/theme > $CONF_DIR/config.d/theme
     fi
 }
 
@@ -132,15 +132,17 @@ main() {
     command_exists "xz" || (echo "xz not found" && exit 1)
 
     local HOME_DIR=${1:-"$HOME"}
-    local CONF_DIR=${2:-"$HOME_DIR/.config/xfce4/xfconf/xfce-perchannel-xml"}
+    local XFCE_CONF_DIR=${2:-"$HOME_DIR/.config/xfce4/xfconf/xfce-perchannel-xml"}
+    local SWAY_CONF_DIR="$HOME_DIR/.config/sway"
 
     mkdir -p $HOME_DIR
-    mkdir -p $CONF_DIR
+    mkdir -p $XFCE_CONF_DIR
+    mkdir -p $SWAY_CONF_DIR
 
     download_resources $HOME_DIR
-    apply_xfce_settings $CONF_DIR
+    apply_xfce_settings $XFCE_CONF_DIR
     apply_tmux_settings $HOME_DIR
-    apply_sway_settings $HOME_DIR
+    apply_sway_settings $SWAY_CONF_DIR
 }
 
 main $*
